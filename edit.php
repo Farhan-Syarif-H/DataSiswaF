288e04a8b9894fcdad55a5aee2965641a56f4539
farhan-syarif-h <farhansyarifhidayatulloh@smkwikrama.sch.id>
2024-05-26 22:35:26 +0700

    first commit

---
+++ edit.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+if (isset($_GET['key'])) {
+    $key = $_GET['key'];
+    if (isset($_SESSION['dataSiswa'][$key])) {
+        $siswa = $_SESSION['dataSiswa'][$key];
+    }
+}
+// mengecek jika request method nya selain dari post maka kode tidak akan dijalankan sc:stackoverflow
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($key)) {
+    // update data siswa
+    $_SESSION['dataSiswa'][$key]['nama'] = $_POST['nama'];
+    $_SESSION['dataSiswa'][$key]['nis'] = $_POST['nis'];
+    $_SESSION['dataSiswa'][$key]['rayon'] = $_POST['rayon'];
+
+    // kembali ke halaman index
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        a {
+            text-decoration:none;
+            background-color:wheat;
+            padding-top:3px;
+            padding-bottom:5px;
+            padding-right:7px;
+            padding-left:7px;
+            border-radius:10px;
+        
+        }
+            
+        
+
+        body{
+            background-color:wheat;
+        }
+
+        .tengah{
+            margin-top:17rem;
+            background-color:cyan;
+            width:400px;
+            margin-left:600px;
+            border-radius:9px;
+            padding-top:15px;
+            padding-bottom:15px;
+        }
+    </style>
+</head>
+<body>
+    <div class="tengah">
+    <center>
+    <form action="" method = "POST">
+        
+                                    <label for="nama">Nama:</label><br>
+                                    <input type="text" id="nama" name="nama" class=""
+                                        value="<?php echo $siswa['nama']; ?>"><br>
+                                    <label for="nis">NIS:</label><br>
+                                    <input type="text" id="nis" name="nis" value="<?php echo $siswa['nis']; ?>"><br>
+                                    <label for="rayon">Rayon:</label><br>
+                                    <input type="text" id="rayon" name="rayon"
+                                        value="<?php echo $siswa['rayon']; ?>"><br>
+                                        <br>
+
+                                        <div>
+                                        <input type="submit" value="Simpan">
+                                    <a href="index.php">Kembali</a>
+                                        </div>
+                                        </form>
+                                        </center>
+                                        </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,121 @@
+<?php 
+session_start();
+// isialisasi array kosong 
+if (!isset($_SESSION['dataSiswa'])) {
+    $_SESSION['dataSiswa'] = array();
+}
+
+if (isset($_POST["tambah"])) {
+    if ($_POST['nama'] == "" && $_POST['nis'] == "" && $_POST['rayon'] == "") {
+        echo "data kosong <br>";
+    } else {
+        $siswa = array(
+            "nama"  => $_POST ['nama'],
+            "nis"   => $_POST ['nis'],
+            "rayon" => $_POST ['rayon']
+        );
+        array_push($_SESSION['dataSiswa'], $siswa);
+    }
+}
+
+// hapus data dari session
+if (isset($_GET['hapus'])) {
+    $key = $_GET['hapus'];
+    unset($_SESSION['dataSiswa'][$key]);
+
+    header('location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <div class="image">
+        <img src="imagespb.jpg" alt="">
+        <h1>Farhan Syarif Hidayatulloh</h1>
+    </div>
+    <center>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<style>
+    a {
+        text-decoration: none;
+        color: black;
+    }
+    body {
+        background-color:wheat;
+    }
+.output{
+    background-color:white;
+    width: 250px;
+    border-radius:6px;
+    box-shadow: 2px 2px 2px 0px; 
+}
+
+H1 {
+    background-color:white;
+    width:250px;
+    border-radius:6px;
+    box-shadow: 2px 2px 2px 0px; 
+}
+.image img {
+    width:130px;
+    height:130px;
+    border-radius:50%;
+    margin-left:4rem;
+}
+
+.image h1{
+    text-align:center;
+}
+
+
+
+</style>
+<div class="semua">
+    <h1>DATA SISWA</h1>
+    <form action="" method = "POST">
+    <label for="nama">Nama : </label><br>
+
+    <input type="text" name="nama" id = "nama" required><br>
+
+    <label for="nis">Nis : </label><br>
+
+    <input type="number" name="nis" id="nis" required><br>
+
+    <label for="rayon">Rayon : </label><br>
+
+    <input type="text" name="rayon" id="rayon" required><br>
+
+    <div>
+        <button name = "tambah">Tambah</button>
+        <button name= "reser"><a href="destroy.php">Reset</a></button>
+    </div>
+
+    </form>
+    </div>
+
+<div class="output">
+    <?php
+                            if (!empty($_SESSION['dataSiswa'])) {
+                                foreach ($_SESSION['dataSiswa'] as $key => $value) {
+                                    $nama = $value["nama"];
+                                    $nis = $value["nis"];
+                                    $rayon = $value["rayon"];
+                                    echo ucwords("<h4> $nama | $nis | $rayon</h4>");
+                                    echo "<a href='?hapus=" . $key . "' class=\"btn btn-danger ms-2 mt-2\"><i class='hapus'></i> Hapus</a>";
+                                    echo "<a href='edit.php?key=" . $key . "' class=\"btn btn-primary ms-2 mt-2\"><i class='bx bx-pencil'></i> Edit</a>";
+                                    echo "<br>";
+                                }
+                            }
+                            ?>
+                            </div>
+</body>
+</center>
+</html>
